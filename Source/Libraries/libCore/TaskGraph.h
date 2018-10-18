cc1345b43096ba8bd035472569fa253eb9c4e473
WildPyromancer <pyro@nospam.com>
2018-10-18 00:49:31 -0400

    a little bit more work before I turn in

--- Source/Libraries/libCore/TaskGraph.h
+++ Source/Libraries/libCore/TaskGraph.h
@@ -11,6 +11,8 @@
 #define LIBCORE_TASKGRAPH_H_
 #pragma once
 
+#include "libCore.h"
+
 OPEN_NAMESPACE(Firestorm);
 
 /**
@@ -24,34 +26,8 @@ class GoWidePool
 
 };
 
-class Node
-{
-	template<class ThreadPool> friend class TaskGraph;
-public:
-	template<class Function>
-	Node(class TaskGraph& graph, size_t index, Function&& func)
-	: _graph(graph)
-	, _index(index)
-	, _operation(forward<Function>(func))
-	{
-	}
+using Node = size_t;
 
-	/**
-		Invoke the held operation.
-	 **/
-	inline void Invoke() const
-	{
-		_operation();
-	}
-
-private:
-	class TaskGraph& _graph;
-	size_t _index;
-	function<void()> _operation;
-	vector<size_t> _children;
-};
-
-template<class ThreadPool>
 class TaskGraph final
 {
 public:
@@ -61,17 +37,18 @@ public:
 		Push a new node into the queue.
 	 **/
 	template<class Function>
-	Node& Push(Function&& function)
+	Node Push(Function&& function)
 	{
 		size_t index = _nodes.size();
-		_nodes.push_back(Node<Function>(forward<Function>(function)));
-		return _nodes.back();
+		_nodeFuncs.push_back(forward<Function>(function));
+		_nodeChildren.push_back(vector<Node>());
+		return index;
 	}
 
 	/**
 		Check if two nodes are linked.
 	 **/
-	bool AreLinked(const Node& from, const Node& to);
+	bool AreLinked(Node from, Node to);
 
 	/**
 		Clear the work queue and end the task tree. Calls ThreadPool::Join.
@@ -79,13 +56,8 @@ public:
 	void Shutdown();
 
 private:
-	friend class Node;
-
 	// connects from->to in the graph.
-	void Link(const Node& from, const Node& to);
-	Node& GetNode(size_t index);
-
-	ThreadPool _threadPool;
+	void Link(Node from, Node to);
 
 	string _name;
 	mutex _queueLock;
@@ -95,47 +67,36 @@ private:
 	// std::condition_variable _cv;
 	// bool _quit{ false };
 
-	vector<Node>                 _nodes;
-	vector<pair<size_t, size_t>> _edges;
+	vector<function<void()>>         _nodeFuncs;    // functions for the nodes.
+	vector<pair<Node, Node>>         _edges;        // edges between nodes.
+	vector<pair<Node, vector<Node>>> _nodeChildren; // children of the nodes.
 };
 
-template<class ThreadPool>
-TaskGraph<ThreadPool>::TaskGraph()
+TaskGraph::TaskGraph()
 {
 }
 
-template<class ThreadPool>
-bool TaskGraph<ThreadPool>::AreLinked(const Node& from, const Node& to)
+bool TaskGraph::AreLinked(Node from, Node to)
 {
 	for(const auto& edge : _edges)
 	{
 		// check if they're already linked.
-		if(edge.first == from._index)
-			if(edge.second == to._index)
+		if(edge.first == from)
+			if(edge.second == to)
 				return true;
 	}
 	return false;
 }
 
-template<class ThreadPool>
-void TaskGraph<ThreadPool>::Shutdown()
+void TaskGraph::Shutdown()
 {
-	_threadPool.Join();
 }
 
-template<class ThreadPool>
-void TaskGraph<ThreadPool>::Link(const Node& from, const Node& to)
+void TaskGraph::Link(Node from, Node to)
 {
 	if(AreLinked(from, to))
 		return; // no-op if the nodes are already linked.
-	_edges.push_back(from._index, to._index);
-}
-
-template<class ThreadPool>
-Node& TaskGraph<ThreadPool>::GetNode(size_t index)
-{
-	FIRE_ASSERT(index < _nodes.size());
-	return _nodes[index];
+	_edges.push_back(pair(from, to));
 }
 
 CLOSE_NAMESPACE(Firestorm);
